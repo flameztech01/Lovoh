4555609f92a49f8d1781064b2b7959ebc76552fd
flameztech01 <flameztech01@gmail.com>
2025-10-25 12:31:52 +0100

    worked on baseUrl

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
-const baseQuery =  import.meta.env.VITE_API_URL || '/api';
+
+const baseQuery =  fetchBaseQuery({
+   baseUrl: import.meta.env.VITE_API_URL || '/api'
+});
 
 export const apiSlice = createApi({
     baseQuery,
