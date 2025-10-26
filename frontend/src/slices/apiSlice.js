aba3725e7e972e724e67430e082bb34f553aa411
flameztech01 <flameztech01@gmail.com>
2025-10-26 15:48:13 +0100

    added credentials to apiSlice

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,20 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({
-  baseUrl: `${import.meta.env.VITE_API_URL || ''}/api`,
-  prepareHeaders: (headers, { getState }) => {
-    const adminInfo = getState().auth.adminInfo;
 
-    if (adminInfo?.token) {
-      headers.set('Authorization', `Bearer ${adminInfo.token}`);
-    }
 
-    return headers;
-  },
+const baseQuery =  fetchBaseQuery({
+   baseUrl: `${import.meta.env.VITE_API_URL || ''}/api`,
+   credentials: 'include',
 });
 
 export const apiSlice = createApi({
-  baseQuery,
-  tagTypes: ['user', 'admin'],
-  endpoints: (builder) => ({}),
-});
+    baseQuery,
+    tagTypes: ['user', 'admin'],
+    endpoints: (builder) => ({})
+}); 
