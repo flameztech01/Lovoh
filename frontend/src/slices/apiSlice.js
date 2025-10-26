7eb94cd506f1ba37aece8132cb3e9195408d0135
flameztech01 <flameztech01@gmail.com>
2025-10-26 15:18:31 +0100

    Added authorization

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,13 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const baseQuery = fetchBaseQuery({
+  baseUrl: `${import.meta.env.VITE_API_URL || ''}/api`,
+  prepareHeaders: (headers, { getState }) => {
+    const adminInfo = getState().auth.adminInfo;
 
+    if (adminInfo?.token) {
+      headers.set('Authorization', `Bearer ${adminInfo.token}`);
+    }
 
-const baseQuery =  fetchBaseQuery({
-   baseUrl: `${import.meta.env.VITE_API_URL || ''}/api`
+    return headers;
+  },
 });
 
 export const apiSlice = createApi({
-    baseQuery,
-    tagTypes: ['user', 'admin'],
-    endpoints: (builder) => ({})
-}); 
+  baseQuery,
+  tagTypes: ['user', 'admin'],
+  endpoints: (builder) => ({}),
+});
