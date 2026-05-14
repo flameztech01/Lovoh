1115aedf3e2502fa3a6cc0af7c7a99b7837b14b1
flameztech01 <flameztech01@gmail.com>
2026-05-14 10:49:27 +0100

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        biizzed: resolve(__dirname, 'biizzed.html'),
+        uduua: resolve(__dirname, 'uduua.html'),
+        eventroom: resolve(__dirname, 'eventroom.html'),
+      },
+    },
+  },
   server: {
     host: true,
     port: 9000,
@@ -16,7 +26,4 @@ export default defineConfig({
       }
     }
   },
-  
-})
-
-
+});
